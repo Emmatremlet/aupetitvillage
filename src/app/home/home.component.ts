c286e5afe22422430b96e3ae812af254d357efaf
Emma TREMLET <elecuelle001@ensc.fr>
2023-10-13 22:08:13 +0200

    Fonction permettant de trier par ordre croissant ou décroissant les produits

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,17 +11,25 @@ export class HomeComponent {
   AsterixObelix: any = "../assets/asterixobelix.jpg"
 
   products: Product[];
+  sortedProducts: Product[];
+  sortDirection: string = 'asc'; // Par défaut, tri croissant
 
   constructor(private productService: ProductService) {
     this.products = productService.getProducts();
+    this.sortedProducts = [...this.products]; // Copie de la liste non triée
   }
 
-  sortProductsByNameAscending() {
-    this.products = this.productService.sortProductsByNameAscending();
+  sortProducts(event: Event) {
+    const target = event.target as HTMLSelectElement;
+    this.sortDirection = target.value;
+
+    this.sortedProducts = this.products.slice(); // Créez une copie pour éviter de modifier l'original
+
+    if (this.sortDirection === 'asc') {
+      this.sortedProducts.sort((a, b) => a.name.localeCompare(b.name));
+    } else if (this.sortDirection === 'desc') {
+      this.sortedProducts.sort((a, b) => b.name.localeCompare(a.name));
+    }
   }
 
-  sortProductsByNameDescending() {
-    this.products = this.productService.sortProductsByNameDescending();
-  }
-  
 }
