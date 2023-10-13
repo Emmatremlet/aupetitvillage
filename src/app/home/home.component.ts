fb584b60c78d33c21d1570202da5e0b1d5d158b3
Emma TREMLET <elecuelle001@ensc.fr>
2023-10-13 22:49:15 +0200

    Ajout de la fonction rechercher un produit par nom

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,28 +8,38 @@ import { Product } from '../product.model';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  AsterixObelix: any = "../assets/asterixobelix.jpg"
+  AsterixObelix: any = "../assets/asterixobelix.jpg";
+  Loupe: any = "../assets/loupe.png"
 
   products: Product[];
   sortedProducts: Product[];
-  sortDirection: string = 'asc'; // Par défaut, tri croissant
+  sortDirection: string = 'asc';
+  searchTerm: string = '';
 
   constructor(private productService: ProductService) {
     this.products = productService.getProducts();
-    this.sortedProducts = [...this.products]; // Copie de la liste non triée
+    this.sortedProducts = [...this.products];
   }
 
   sortProducts(event: Event) {
     const target = event.target as HTMLSelectElement;
     this.sortDirection = target.value;
 
-    this.sortedProducts = this.products.slice(); // Créez une copie pour éviter de modifier l'original
+    this.sortedProducts = this.products.slice();
 
     if (this.sortDirection === 'asc') {
       this.sortedProducts.sort((a, b) => a.name.localeCompare(b.name));
     } else if (this.sortDirection === 'desc') {
       this.sortedProducts.sort((a, b) => b.name.localeCompare(a.name));
     }
+  }
+
+  searchProducts(event: Event) {
+    const target = event.target as HTMLSelectElement;
+    this.searchTerm = target.value;
+    this.sortedProducts = this.products.filter(product =>
+      product.name.toLowerCase().includes(target.value.toLowerCase())
+    );
   }
 
 }
